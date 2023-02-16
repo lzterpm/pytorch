994905f22d2dd4a2eac89be2995b3a873557047a
Nikita Vedeneev <nik@quansight.com>
2023-02-16 17:51:42 +0000

    sparse.mm backward: performance improvements
    ghstack-source-id: 1526dd8e9f92d7f02bbcadf899fe7840d007982f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94991

--- torch/csrc/autograd/FunctionsManual.cpp
+++ torch/csrc/autograd/FunctionsManual.cpp
@@ -1370,19 +1370,13 @@ Tensor sparse_sparse_matmul_backward(
   TORCH_CHECK(
       grad_order == 0 || grad_order == 1,
       ": grad_order not in [0, 1] at sparse_sparse_matmul_backward function");
-  const auto mask_ones_like = [](const Tensor& t) -> Tensor {
-    return at::sparse_coo_tensor(
-        t._indices(),
-        at::ones({1}, t._values().options()).expand_as(t._values()),
-        t.sizes());
-  };
 
   if (grad_order == 0) {
     auto a_grad = _sparse_sparse_matmul(grad, b.conj().t());
-    return a_grad.mul(mask_ones_like(a.coalesce()));
+    return a_grad.sparse_mask(a.coalesce());
   }
   auto b_grad = _sparse_sparse_matmul(a.conj().t(), grad);
-  return b_grad.mul(mask_ones_like(b.coalesce()));
+  return b_grad.sparse_mask(b.coalesce());
 }
 
 Tensor renorm_backward(
